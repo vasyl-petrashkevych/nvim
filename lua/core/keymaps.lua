85cd0557c450d2a6eb0dd4e7e7de73ad6100553d
Vasyl Petrashkevych <petrashkevych.v@gmail.com>
2024-01-11 14:47:19 +0200

    Add tmux keys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,3 +18,8 @@ keymap.set('i', '<C-k>', '<up>')
 keymap.set('i', '<C-j>', '<down>')
 
 keymap.set({ 'i', 'n' }, '<C-s>', "<esc>:w<CR>")
+-- Tmux --
+keymap.set("n", "<C-h>", "<cmd>TmuxNavigateRight<cr>")
+keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+keymap.set("n", "<C-l>", "<cmd>TmuxNavigateLeft<cr>")
--- lua/plugins/vim-tmux.lua
+++ lua/plugins/vim-tmux.lua
@@ -16,9 +16,9 @@ return {
 				hashfile = vim.fn.stdpath("data") .. "/config-local",
 
 				autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
-				commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
-				silent = false,             -- Disable plugin messages (Config loaded/ignored)
-				lookup_parents = false,     -- Lookup config files in parent directories
+				commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+				silent = false,         -- Disable plugin messages (Config loaded/ignored)
+				lookup_parents = false, -- Lookup config files in parent directories
 			})
 		end,
 	},
